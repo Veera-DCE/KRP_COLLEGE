1c2364a38072f75e676952e08448a81723ca6fd9
Veera-DCE <veeramani.dcetechnology@gmail.com>
2023-02-01 14:11:09 +0530

    Config for server

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_NAME', 'krp');
+define('DB_USERNAME','dceadmin');
+define('DB_PASSWORD','dcedatabase');
+define('DB_NAME', 'krp_fee');
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if( mysqli_connect_error()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 ?>
